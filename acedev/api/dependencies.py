86bd0ac52fed072a04216414262392b3bbdb33ef
Art Moskvin <amoskvin@me.com>
2024-02-27 20:43:24 +0100

    Remove invalid dependency

--- acedev/api/dependencies.py
+++ acedev/api/dependencies.py
@@ -2,7 +2,6 @@ from github import GithubIntegration
 from starlette.requests import Request
 
 from acedev.api.settings import ApiSettings
-from acedev.agent import OpenAIAgent
 from acedev.service.openai_service import OpenAIService
 
 
@@ -12,10 +11,6 @@ def get_ghe_client(request: Request) -> GithubIntegration:
 
 def get_openai_service(request: Request) -> OpenAIService:
     return request.app.state.openai_service  # type: ignore[no-any-return]
-
-
-def get_openai_agent(request: Request) -> OpenAIAgent:
-    return request.app.state.openai_agent  # type: ignore[no-any-return]
 
 
 def get_api_settings(request: Request) -> ApiSettings:
